451d51d2f2d3eed23327f13f0962fc0eeed4454d
Jyoti Deshmukh <jyotipawar2003@gmail.com>
2021-04-20 23:24:20 +0530

    added first twig template, used blocks, extended template

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -23,8 +24,21 @@ class QuestionController
      */
 
     public function show($slug){
-        return new Response(sprintf(" My question is '%s'",
+        /*
+         return new Response(sprintf(" My question is '%s'",
             str_replace("-"," ",$slug)));
+        */
+        $answers = [
+            'Study hard',
+            'practice it',
+            'understand consepts'
+        ];
+        return $this->render('question/show.html.twig',
+        [
+            'question'=>str_replace("-"," ",$slug),
+            'answers'=>$answers
+        ]);
+
 
     }
 }
