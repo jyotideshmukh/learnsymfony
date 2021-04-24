48dc346961e585424b5c5582108f1fcb3226a6bb
Jyoti Deshmukh <jyotipawar2003@gmail.com>
2021-04-24 14:07:03 +0530

    added logger interface as service

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 
 namespace App\Controller;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,17 +11,19 @@ class CommentController extends AbstractController
 {
 
     /**
-     * @Route("/comments/{id}/vote/{direction}")
+     * @Route("/comments/{id<\d+>}/vote/{direction<up|down>}", methods="POST")
      * @param $id
      * @param $direction
      * @return JsonResponse
      */
-    public function vote($id, $direction){
+    public function vote($id, $direction, LoggerInterface $logger){
 
         if($direction == 'up'){
+            $logger->info('voting up');
             return new JsonResponse(['vote'=>rand(7,100)]);
         }
         else{
+            $logger->info('voting down');
             return new JsonResponse(['vote'=>rand(0,5)]);
         }
     }
