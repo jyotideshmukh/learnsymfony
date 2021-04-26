0d860520bde8bc3def510b7725c1ae3f9ce3aa33
Jyoti Deshmukh <jyotipawar2003@gmail.com>
2021-04-26 12:27:44 +0530

    tested cache and markdown services

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,12 @@
 namespace App\Controller;
 
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -19,7 +21,7 @@ class QuestionController extends AbstractController
 
     public function homepage(){
         $questions = [
-            'How to pass **devops**',
+            'How to pass devops',
             'How to be safe in pandemic',
             'How to increase hemoglobin'
         ];
@@ -35,16 +37,22 @@ class QuestionController extends AbstractController
      * @Route("/question/{slug}",name="app_question_answer")
      */
 
-    public function show($slug){
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache){
         /*
          return new Response(sprintf(" My question is '%s'",
             str_replace("-"," ",$slug)));
         */
-        $answers = [
-            'Study hard',
-            'practice it',
-            'understand concepts'
-        ];
+        $answers = $cache->get('answers',function() use ($markdownParser){
+            $answers = [
+                $markdownParser->transformMarkdown('**Study hard**'),
+                $markdownParser->transformMarkdown('**practice** it'),
+                $markdownParser->transformMarkdown('**understand** concepts')
+            ];
+            return $answers;
+        });
+        dd($markdownParser);
+
+
         return $this->render('question/show.html.twig',
         [
             'question'=>str_replace("-"," ",$slug),
