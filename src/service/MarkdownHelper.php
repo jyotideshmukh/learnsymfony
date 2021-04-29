52c29518fa8b11180c26f68eabe60485400be5bc
Jyoti Deshmukh <jyotipawar2003@gmail.com>
2021-04-29 19:18:01 +0530

    added sentry and stored value of SENTRY_DSN in secret valut

--- src/service/MarkdownHelper.php
+++ src/service/MarkdownHelper.php
@@ -5,6 +5,7 @@ namespace App\service;
 
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Sentry\State\HubInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class MarkdownHelper
@@ -17,18 +18,23 @@ class MarkdownHelper
      * MarkdownHelper constructor.
      */
     public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache,
-                                bool $isDebug)
+                                bool $isDebug, HubInterface $hub)
     {
         $this->markDownParser = $markdownParser;
         $this->cache = $cache;
         $this->isDebug = $isDebug;
+        //dd($hub);
 
     }
 
     public function parse(string $source){
         //dd($this->isDebug);
+        if(!$this->isDebug){
+            return $this->markDownParser->transformMarkdown($source);
+        }
+        //throw new \Exception("Bad stuff happened");
             return $this->cache->get('markdown_'.md5($source),function() use ($source){
                    return $this->markDownParser->transformMarkdown($source);
             });
-        }
+    }
 }
