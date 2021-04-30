26fee59d640eb1fb7a7b6255d78e2eff34dd2d2c
jyotideshmukh <jyotipawar2003@gmail.com>
2021-04-30 15:00:22 +0530

    done migrations and created questions and fetched from database

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,8 +4,11 @@
 namespace App\Controller;
 
 
+use App\Entity\Question;
 use App\service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 #use Twig\Environment;
 
@@ -16,12 +19,18 @@ class QuestionController extends AbstractController
      */
 
 
-    public function homepage(){
-        $questions = [
+    public function homepage(EntityManagerInterface $entityManager){
+        $repository = $entityManager->getRepository(Question::class);
+        $questions = $repository->findAll();
+        foreach($questions as $question){
+            $questionsArr[] = $question->getSlug();
+        }
+        $questions = $questionsArr;
+        /*$questions = [
             'How to pass devops',
             'How to be safe in pandemic',
             'How to increase hemoglobin'
-        ];
+        ];*/
        /* $html = $twigEnvironment->render('question/homepage.html.twig',
             ['questions'=>$questions]);
         return new Response($html);*/
@@ -31,15 +40,40 @@ class QuestionController extends AbstractController
     }
 
     /**
-     * @Route("/question/{slug}",name="app_question_answer")
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager){
+        $question = new Question();
+        $question->setName('Devops preparation');
+        $question->setSlug('devops-preparation-'.rand(1,1000));
+        $question->setQuestion('How to pass Devops?');
+        if(rand(1,10)> 2){
+            $question->setAskedAt(new \DateTime());
+        }
+        $entityManager->persist($question);
+        $entityManager->flush();
+        return new Response(sprintf('question id is %d and question slug is %s',
+            $question->getId(),$question->getSlug()));
+    }
+
+    /**
+     * @Route("/questions/{slug}",name="app_question_answer")
      */
 
-    public function show($slug, MarkdownHelper $markdownHelper){
+    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager){
         /*
          return new Response(sprintf(" My question is '%s'",
             str_replace("-"," ",$slug)));
         */
 
+        $repository = $entityManager->getRepository(Question::class);
+        /**  var Question|null $question */
+        $question = $repository->findOneBy(['slug'=>$slug]);
+        if(!$question){
+            throw $this->createNotFoundException(
+                sprintf('Given slug not found "%s"', $slug)
+            );
+        }
             $answers = [
                 $markdownHelper->parse('**Study hard**'),
                 $markdownHelper->parse('**practice** it'),
@@ -52,7 +86,8 @@ class QuestionController extends AbstractController
 
         return $this->render('question/show.html.twig',
         [
-            'question'=>str_replace("-"," ",$slug),
+           // 'question'=>str_replace("-"," ",$slug),
+            'question'=>$slug,
             'answers'=>$answers
         ]);
 
