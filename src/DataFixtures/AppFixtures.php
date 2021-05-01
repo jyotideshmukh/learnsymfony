c5e3e67f70442fb5d235575f5c2dace741a036ec
jyotideshmukh <jyotipawar2003@gmail.com>
2021-05-01 15:32:09 +0530

    added foundry and tested questions fixures with foundry

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Question;
+use App\Factory\QuestionFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+       /* $question = new Question();
+        $question->setName('Learn Fixures');
+        $question->setSlug('learn-fixures-'.rand(1,1000));
+        $question->setQuestion('How to learn Fixures- using orm-fixures?');
+        if(rand(1,10)> 2){
+            $question->setAskedAt(new \DateTime());
+        }
+         $manager->persist($question);*/
+
+        QuestionFactory::new()->createMany(20);
+        $manager->flush();
+
+        $manager->flush();
+    }
+}
---
+++ src/Factory/QuestionFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @method static Question|Proxy createOne(array $attributes = [])
+ * @method static Question[]|Proxy[] createMany(int $number, $attributes = [])
+ * @method static Question|Proxy find($criteria)
+ * @method static Question|Proxy findOrCreate(array $attributes)
+ * @method static Question|Proxy first(string $sortedField = 'id')
+ * @method static Question|Proxy last(string $sortedField = 'id')
+ * @method static Question|Proxy random(array $attributes = [])
+ * @method static Question|Proxy randomOrCreate(array $attributes = [])
+ * @method static Question[]|Proxy[] all()
+ * @method static Question[]|Proxy[] findBy(array $attributes)
+ * @method static Question[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Question[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static QuestionRepository|RepositoryProxy repository()
+ * @method Question|Proxy create($attributes = [])
+ */
+final class QuestionFactory extends ModelFactory
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        // TODO inject services if required (https://github.com/zenstruck/foundry#factories-as-services)
+    }
+
+    protected function getDefaults(): array
+    {
+        return [
+            'name'=>'Learn Fixures',
+            'slug'=>'learn-fixures-'.rand(1,1000),
+            'question'=>'How to learn Fixures- using orm-fixures?',
+            'askedAt'=>new \DateTime()
+            // TODO add your default values here (https://github.com/zenstruck/foundry#model-factories)
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://github.com/zenstruck/foundry#initialization
+        return $this
+            // ->afterInstantiate(function(Question $question) {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Question::class;
+    }
+}
